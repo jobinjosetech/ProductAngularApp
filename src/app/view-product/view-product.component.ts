79c9780e58fe325bf18d71cb392b8d1cf87f0720
jobin <jobinjose.in@gmail.com>
2022-12-07 07:33:29 +0530

    apis

--- src/app/view-product/view-product.component.ts
+++ src/app/view-product/view-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-product',
@@ -9,6 +10,7 @@ export class ViewProductComponent {
   constructor(private api:ApiService){
     api.fetchData().subscribe(
       (response)=>{
+        console.log(response)
         this.products = response
       }
     )
